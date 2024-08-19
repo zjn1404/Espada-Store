88ab072cbfdae87983606451c5f30c14b2bca963
Zjn <129529039+zjn1404@users.noreply.github.com>
2024-08-19 18:40:27 +0700

    Add unit test for usercontroller

--- backend/spring-boot-espada-store/src/main/java/com/nqt/spring_boot_espada_store/exception/ErrorCode.java
+++ backend/spring-boot-espada-store/src/main/java/com/nqt/spring_boot_espada_store/exception/ErrorCode.java
@@ -10,38 +10,45 @@ import lombok.Getter;
 public enum ErrorCode {
     // Undefined error
     UNCATEGORIZED_EXCEPTION(9999, HttpStatus.INTERNAL_SERVER_ERROR, "Uncategorized Error"),
-    // Developer error
+
+    // Developer error (1xxx)
     INVALID_KEY(1001, HttpStatus.INTERNAL_SERVER_ERROR, "Uncategorized error"),
+
     // Client error
-    USER_EXISTED(1002, HttpStatus.BAD_REQUEST, "User Existed"),
-    USERNAME_INVALID(1003, HttpStatus.BAD_REQUEST, "Username must be at least 4 characters"),
-    PASSWORD_INVALID(1004, HttpStatus.BAD_REQUEST, "Password must be at least 8 characters"),
-    USER_NOT_EXISTED(1005, HttpStatus.BAD_REQUEST, "User Not Existed"),
-    UNAUTHENTICATED(1006, HttpStatus.UNAUTHORIZED, "Authentication Failed"),
-    UNAUTHORIZED(1007, HttpStatus.FORBIDDEN, "Don't have permission"),
-    INVALID_TOKEN(1008, HttpStatus.BAD_REQUEST, "Invalid Token"),
-    TYPE_NOT_EXISTED(1009, HttpStatus.BAD_REQUEST, "Type Not Existed"),
-    SUBTYPE_NOT_EXISTED(1010, HttpStatus.BAD_REQUEST, "SubType Not Existed"),
-    PRODUCT_NOT_EXISTED(1011, HttpStatus.BAD_REQUEST, "Product Not Existed"),
-    TYPE_EXISTED(1012, HttpStatus.BAD_REQUEST, "Type Existed"),
-    SUB_TYPE_EXISTED(1013, HttpStatus.BAD_REQUEST, "Sub Type Existed"),
-    PRODUCT_EXISTED(1014, HttpStatus.BAD_REQUEST, "Product Existed"),
-    ROLE_NOT_EXISTED(1015, HttpStatus.BAD_REQUEST, "Role Not Existed"),
-    CUSTOMER_DETAIL_EXISTED(1016, HttpStatus.BAD_REQUEST, "Customer Detail Existed"),
-    CUSTOMER_DETAIL_NOT_EXISTED(1017, HttpStatus.BAD_REQUEST, "Customer Detail Not Existed"),
-    USER_NOT_VERIFIED(1018, HttpStatus.BAD_REQUEST, "User Not Verified"),
-    VERIFY_CODE_INCORRECT(1019, HttpStatus.BAD_REQUEST, "Verify Code Incorrect"),
-    VERIFY_CODE_EXPIRED(1020, HttpStatus.BAD_REQUEST, "Verify Code Expired, New Code Has Been Sent"),
-    ACCOUNT_NOT_VERIFIED(1021, HttpStatus.BAD_REQUEST, "Account Not Verified"),
-    PASSWORD_INCORRECT(1022, HttpStatus.BAD_REQUEST, "Password Incorrect"),
-    THE_SAME_PASSWORD(1023, HttpStatus.BAD_REQUEST, "New Password Is The Same As Old Password"),
-    QUANTITY_INVALID(1024, HttpStatus.BAD_REQUEST, "Quantity MUST BE AT LEAST 1"),
-    PAYMENT_INVALID(1025, HttpStatus.BAD_REQUEST, "Payment MUST BE AT LEAST 0"),
-    CART_NOT_EXISTED(1026, HttpStatus.BAD_REQUEST, "Cart Not Existed"),
-    CART_DETAIL_NOT_EXISTED(1027, HttpStatus.BAD_REQUEST, "Cart Detail Not Existed"),
-    ORDER_NOT_EXISTED(1028, HttpStatus.BAD_REQUEST, "Order Not Existed"),
-    QUANTITY_GREATER_THAN_STOCK(1029, HttpStatus.BAD_REQUEST, "Quantity is greater than stock"),
-    PAYMENT_NOT_SUCCEED(1030, HttpStatus.BAD_REQUEST, "Payment Not Succeed"),
+    // Input error (2xxx)
+    USERNAME_INVALID(2001, HttpStatus.BAD_REQUEST, "Username must be at least 4 characters"),
+    PASSWORD_INVALID(2002, HttpStatus.BAD_REQUEST, "Password must be at least 8 characters"),
+    THE_SAME_PASSWORD(2003, HttpStatus.BAD_REQUEST, "New Password Is The Same As Old Password"),
+    PAYMENT_INVALID(2004, HttpStatus.BAD_REQUEST, "Payment MUST BE AT LEAST 0"),
+    QUANTITY_INVALID(2005, HttpStatus.BAD_REQUEST, "Quantity MUST BE AT LEAST 1"),
+    QUANTITY_GREATER_THAN_STOCK(2006, HttpStatus.BAD_REQUEST, "Quantity is greater than stock"),
+    VERIFY_CODE_INCORRECT(2007, HttpStatus.BAD_REQUEST, "Verify Code Incorrect"),
+    // Existed error (2xxx)
+    USER_EXISTED(2008, HttpStatus.BAD_REQUEST, "User Existed"),
+    TYPE_EXISTED(2009, HttpStatus.BAD_REQUEST, "Type Existed"),
+    SUB_TYPE_EXISTED(2010, HttpStatus.BAD_REQUEST, "Sub Type Existed"),
+    PRODUCT_EXISTED(2011, HttpStatus.BAD_REQUEST, "Product Existed"),
+    CUSTOMER_DETAIL_EXISTED(2012, HttpStatus.BAD_REQUEST, "Customer Detail Existed"),
+    VERIFY_CODE_EXPIRED(2013, HttpStatus.BAD_REQUEST, "Verify Code Expired, New Code Has Been Sent"),
+    // Other (2xxx)
+    PAYMENT_NOT_SUCCEED(2014, HttpStatus.BAD_REQUEST, "Payment Not Succeed"),
+    // Unauthenticated error (3xxx)
+    UNAUTHENTICATED(3001, HttpStatus.UNAUTHORIZED, "Authentication Failed"),
+    INVALID_TOKEN(3002, HttpStatus.UNAUTHORIZED, "Invalid Token"),
+    ACCOUNT_NOT_VERIFIED(3003, HttpStatus.UNAUTHORIZED, "Account Not Verified"),
+    PASSWORD_INCORRECT(3004, HttpStatus.UNAUTHORIZED, "Password Incorrect"),
+    // Unauthorized error (4xxx)
+    UNAUTHORIZED(4001, HttpStatus.FORBIDDEN, "Don't have permission"),
+    // Not Found error (5xxx)
+    USER_NOT_EXISTED(5001, HttpStatus.NOT_FOUND, "User Not Existed"),
+    TYPE_NOT_EXISTED(5002, HttpStatus.NOT_FOUND, "Type Not Existed"),
+    SUBTYPE_NOT_EXISTED(5003, HttpStatus.NOT_FOUND, "SubType Not Existed"),
+    PRODUCT_NOT_EXISTED(5004, HttpStatus.NOT_FOUND, "Product Not Existed"),
+    ROLE_NOT_EXISTED(5005, HttpStatus.NOT_FOUND, "Role Not Existed"),
+    CUSTOMER_DETAIL_NOT_EXISTED(5006, HttpStatus.NOT_FOUND, "Customer Detail Not Existed"),
+    CART_NOT_EXISTED(5007, HttpStatus.NOT_FOUND, "Cart Not Existed"),
+    CART_DETAIL_NOT_EXISTED(5008, HttpStatus.NOT_FOUND, "Cart Detail Not Existed"),
+    ORDER_NOT_EXISTED(5009, HttpStatus.NOT_FOUND, "Order Not Existed"),
     ;
 
     final int code;
--- backend/spring-boot-espada-store/src/test/java/com/nqt/spring_boot_espada_store/controller/UserControllerTest.java
+++ backend/spring-boot-espada-store/src/test/java/com/nqt/spring_boot_espada_store/controller/UserControllerTest.java
@@ -1,2 +1,228 @@
-package com.nqt.spring_boot_espada_store.controller;public class UserControllerTest {
+package com.nqt.spring_boot_espada_store.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nqt.spring_boot_espada_store.dto.request.user.UserCreationRequest;
+import com.nqt.spring_boot_espada_store.dto.request.user.UserUpdateRequest;
+import com.nqt.spring_boot_espada_store.dto.response.UserResponse;
+import com.nqt.spring_boot_espada_store.service.user.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("/test.yml")
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private UserCreationRequest userCreationRequest;
+    private UserCreationRequest userCreationRequestWithUsernameInvalid;
+    private UserCreationRequest userCreationRequestWithPasswordInvalid;
+    private UserUpdateRequest userUpdateRequest;
+    private UserResponse userResponse;
+    private Set<String> roles = new HashSet<>();
+
+    @BeforeEach
+    void initData() {
+        roles.add("USER");
+
+        userCreationRequest = UserCreationRequest.builder()
+                .username("tuong14")
+                .password("Tuong123")
+                .email("tuong14@gmail.com")
+                .firstName("Nguyen")
+                .lastName("Tuong")
+                .phoneNumber("0912345678")
+                .roles(roles)
+                .build();
+
+        userCreationRequestWithUsernameInvalid = UserCreationRequest.builder()
+                .username("tu")
+                .password("Tuong123")
+                .email("tuong14@gmail.com")
+                .firstName("Nguyen")
+                .lastName("Tuong")
+                .phoneNumber("0912345678")
+                .roles(roles)
+                .build();
+
+        userCreationRequestWithPasswordInvalid = UserCreationRequest.builder()
+                .username("tuong14")
+                .password("123")
+                .email("tuong14@gmail.com")
+                .firstName("Nguyen")
+                .lastName("Tuong")
+                .phoneNumber("0912345678")
+                .roles(roles)
+                .build();
+
+        userUpdateRequest = UserUpdateRequest.builder()
+                .password("Tuong123")
+                .email("tuong14@gmail.com")
+                .firstName("Nguyen")
+                .lastName("Tuong")
+                .phoneNumber("0912345678")
+                .roles(roles)
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id("tuong140912345678")
+                .username("tuong14")
+                .password("Tuong123")
+                .email("tuong14@gmail.com")
+                .firstName("Nguyen")
+                .lastName("Tuong")
+                .phoneNumber("0912345678")
+                .build();
+    }
+
+    @Test
+    void createUser_validRequest_success() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(userCreationRequest);
+        Mockito.when(userService.createUser(ArgumentMatchers.any())).thenReturn(userResponse);
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/user")
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("1000"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.id").value("tuong140912345678"));
+    }
+
+    @Test
+    void createUser_usernameInvalid_fail() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(userCreationRequestWithUsernameInvalid);
+
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("2001"));
+    }
+
+    @Test
+    void createUser_passwordInvalid_fail() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(userCreationRequestWithPasswordInvalid);
+
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("2002"));
+    }
+
+    @Test
+    @WithMockUser(username = "tuong14")
+    void updateUser_validRequest_success() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(userUpdateRequest);
+        Mockito.when(userService.updateUser(ArgumentMatchers.any())).thenReturn(userResponse);
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                .put("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("1000"));
+    }
+
+    @Test
+    @WithMockUser(username = "tuong14")
+    void updateUserWithId_validRequest_success() throws Exception {
+        // GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(userUpdateRequest);
+        Mockito.when(userService.updateUser(ArgumentMatchers.eq(userResponse.getId()),ArgumentMatchers.any())).thenReturn(userResponse);
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/user/" + userResponse.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("1000"));
+    }
+
+    @Test
+    @WithMockUser(username = "tuong14")
+    void getUserById_validRequest_success() throws Exception {
+        // GIVEN
+        Mockito.when(userService.getUserById(ArgumentMatchers.any())).thenReturn(userResponse);
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/user/"+userResponse.getId()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("1000"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.id").value("tuong140912345678"));
+    }
+
+    @Test
+    @WithMockUser(username = "tuong14")
+    void getMyInfo_validRequest_success() throws Exception {
+        // GIVEN
+        Mockito.when(userService.getMyInfo()).thenReturn(userResponse);
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/user/my-info"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("1000"))
+                .andExpect(MockMvcResultMatchers.jsonPath("result.id").value("tuong140912345678"));
+    }
+
+    @Test
+    @WithMockUser(username = "tuong14")
+    void getAllUser_validRequest_success() throws Exception {
+        // GIVEN
+        Mockito.when(userService.getUsers()).thenReturn(List.of(userResponse));
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/user"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("1000"));
+    }
+
+    @Test
+    @WithMockUser(username = "tuong14")
+    void deleteUser_validRequest_success() throws Exception {
+        // GIVEN
+        Mockito.doNothing().when(userService).deleteUser(ArgumentMatchers.any());
+        // WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/user/" + userResponse.getId()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("code").value("1000"));
+    }
 }
